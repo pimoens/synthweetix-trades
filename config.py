8a3fee2fc61d5740787c20b5a5500c3509dc71f7
Pieter Moens <pieter@pietermoens.be>
2021-05-21 18:25:47 +0200

    Bugfix in configuration

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ class BaseConfig:
     TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN', default='')
     TWITTER_ACCESS_SECRET = os.getenv('TWITTER_ACCESS_SECRET', default='')
 
-    TRADE_VALUE_THRESHOLD = os.getenv('TRADE_VALUE_THRESHOLD', default=250000)
-    EYE_CATCHER_THRESHOLD = os.getenv('EYECATCHER_VALUE_THRESHOLD', default=1000000)
+    TRADE_VALUE_THRESHOLD = int(os.getenv('TRADE_VALUE_THRESHOLD', default=250000))
+    EYE_CATCHER_THRESHOLD = int(os.getenv('EYECATCHER_VALUE_THRESHOLD', default=1000000))
 
 
 class DevelopmentConfig(BaseConfig):
